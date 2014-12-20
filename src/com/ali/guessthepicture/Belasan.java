076e93505dfa55d342aa5a7116b60111dc0454c6
ali1999 <v.reed26@yahoo.co.id>
2014-12-20 15:51:28 +0700

    Tugas Tebak Gambar

---
+++ src/com/ali/guessthepicture/Belasan.java
@@ -0,0 +1,58 @@
+package com.ali.guessthepicture;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputFilter;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Belasan extends Activity {
+	EditText jawabanTxt;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_belasan);
+		//cari komponen editText1
+				jawabanTxt = (EditText)findViewById(R.id.editText1);
+				//set input huruf besar
+				jawabanTxt.setFilters(new InputFilter[] {new InputFilter.AllCaps()});
+				}
+	public void cekJawaban(View v){
+		String JawabanBenar = "BELASAN KORBAN MATI KELAPARAN";
+		String jawabanUser = jawabanTxt.getText().toString();
+		if(jawabanUser.equals(JawabanBenar)){
+	// pean Toast
+			Toast.makeText(getApplicationContext(),
+					"JAWABAN TEPAT!", Toast.LENGTH_LONG).show();
+			Intent i = new Intent(getApplicationContext(), DarahActivity.class);
+			startActivity(i);
+		}
+		else{
+			//Pesan TOast
+			Toast.makeText(getApplicationContext(), "JAWABAN MASIH SALAH!", Toast.LENGTH_LONG).show();
+		
+		}
+		}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.belasan, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
